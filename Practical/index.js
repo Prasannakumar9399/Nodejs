ab0415367d396791a4fec00255b2af8430cd473a
pmj1996 <prasannakumar.jha@invorion.com>
2023-06-14 19:24:47 +0530

    First commit

---
+++ Practical/index.js
@@ -0,0 +1,10 @@
+// const http = require('http');
+
+// http.createServer((req, res) => {
+//     res.write("<h1>Hello Node js ! I have successfuly installed nodemon</h1>");
+//     res.end();
+//     console.log("Server Started at port 4500")
+// }).listen(4500)
+
+const colors = require('colors');
+console.log('hello'.bgBlue);
